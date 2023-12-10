d5ccd7a332cb6459947e18c50ba647a4ee7bf0a8
yanJuicy <hanjub3@gmail.com>
2023-12-10 17:15:58 +0900

    API 예외 처리 - 스프링 부트 기본 오류 처리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -4,11 +4,10 @@ import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
-    
+
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");

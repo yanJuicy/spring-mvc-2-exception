e63fbdbe65bd746e68845a092eb2529446b02797
yanJuicy <hanjub3@gmail.com>
2023-12-10 14:40:55 +0900

    API 예외 처리 - 시작

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,9 +6,9 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class WebServerCustomer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
-
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
